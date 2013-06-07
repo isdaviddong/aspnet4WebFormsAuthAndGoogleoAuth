14eedb5a40a8e5786cbb94dd86ac63dcecc4394d
David <david@studyhost.com>
2013-06-07 10:19:36 +0800

    first version

---
+++ WebApplication18/GoogleCallBack.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.Membership.OpenAuth;
+
+namespace WebApplication18
+{
+    public partial class GoogleCallBack : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var returnUrl = Request.QueryString["sid"];
+            var authResult = OpenAuth.VerifyAuthentication(returnUrl);
+            if (authResult.IsSuccessful)
+            {
+                System.Web.Security.FormsAuthentication.SetAuthCookie(authResult.UserName, false);
+                Response.Redirect("default.aspx");
+            }
+            else {
+                Response.Write("授權失敗!!!");
+            }
+        }
+    }
+}
---
+++ WebApplication18/default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication18
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(User.Identity.Name))
+                this.Label1.Text = User.Identity.Name;
+            else
+                Response.Redirect("login.aspx");
+        }
+    }
+}
